92c0b40170ddbd973e87b0efe1bd242d9b4f80e6
sushavonb372 <sushavonb372@INPC197TWJ>
2020-02-11 15:27:02 +0530

    Add project files.

---
+++ User.Api/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace User.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(GetDummyUsers());
+        }
+
+        public IEnumerable<User> GetDummyUsers()
+        {
+            var users = new List<User>();
+
+            var user = new User
+            {
+                Id = 1,
+                Name = "Rakesh",
+                Email = "rakesh.jha@zydus.in"
+            };
+
+            users.Add(user);
+            return users;
+        }
+    }
+}
